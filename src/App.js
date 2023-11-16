dc98b301cff2914ae42553cce5061c30830740cd
7azemaamer <zicoaamer@gmail.com>
2023-11-17 00:15:20 +0200

    make it responsive

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, createHashRouter } from 'react-router-dom';
 import Layout from './Components/Layout/Layout';
 import Home from './Components/Home/Home';
 import Summary from './Components/Summary/Summary';
 import NotFound from './Components/NotFound/NotFound';
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AmountContextProvider from './TrackerContext/AmountContext';
 
 export default function App() {
-  const routes = createBrowserRouter([
+  const routes = createHashRouter([
     {path:'/', element: <Layout/>, children:[
       {path: '/', element:<Home/> , index:true},
       {path: 'summary', element:<Summary/>},
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,13 +7,22 @@
     import { AmountContext } from "../../TrackerContext/AmountContext";
 
     export default function Home(){
+        const [phoneMediaQuery, setPhoneMediaQuery] = useState(window.matchMedia("(max-width: 700px)"));
 
         let {storedData, setStoredData} = useContext(AmountContext);
 
         const [editingIndex, setEditingIndex] = useState(null);
 
-        useEffect(() => {  
-        }, []);
+        useEffect(() => {
+            const handleResize = () => {
+              setPhoneMediaQuery(window.matchMedia("(max-width: 700px)"));
+            };
+            window.addEventListener('resize', handleResize);
+            return () => {
+              window.removeEventListener('resize', handleResize);
+            };
+          }, [storedData]);
+
         const showDescription = (getParagrapgh)=>{
             Swal.fire(getParagrapgh);
         }
@@ -85,22 +94,24 @@
             }
             return showData.map((record, index) => (
                 <div className="row px-4 py-2" key={`R${index}`}>
-                    <div className="col-md-2 text-center">
+                    <div className="col-md-2 col-3 text-center">
                         <p className="mb-0">{record.amount}<span className="fw-bold fs-6 ps-1 text-success">$</span></p>
                     </div>
-                    <div className="col-md-2 text-center">
+                    <div className="col-md-2 col-3 text-center">
                         <p className="mb-0">{record.category}</p>
                     </div>
-                    <div className="col-md-2 text-center">
-                        <p className="mb-0">{record.date}</p>
-                    </div>
-                    <div className="col-md-2 text-center">
-                        <p className="mb-0 clickable" onClick={() => showDescription(record.description)}>{record.description.split(' ').slice(0, 8).join(' ') + '...'}</p>
-                    </div>
-                    <div className="col-md-2 text-center">
+                    {phoneMediaQuery.matches? "" : <>
+                        <div className="col-md-2 col-2 text-center">
+                            <p className="mb-0">{record.date}</p>
+                        </div>
+                        <div className="col-md-2 col-2 text-center">
+                            <p className="mb-0 clickable" onClick={() => showDescription(record.description)}>{record.description.split(' ').slice(0, 8).join(' ') + '...'}</p>
+                        </div>
+                    </>}
+                    <div className="col-md-2 col-3 text-center">
                         <button onClick={() => handleDelete(index)} className="btn btn-danger">Delete</button>
                     </div>
-                    <div className="col-md-2 text-center">
+                    <div className="col-md-2 col-3 text-center">
                         <button onClick={() => handleModify(index)} className="btn btn-orange">Modify</button>
                     </div>
                 </div>
@@ -122,8 +133,8 @@
         <>
             {/* Input form to get data from user */}
             <form onSubmit={formik.handleSubmit}>
-            <div className="row">
-                <div className="col-md-2">
+            <div className="row g-4">
+                <div className="col-md-2 col-4">
                 <input
                     className={`form-control ${formik.touched.amount && formik.errors.amount ? 'is-invalid' : ''}`}
                     type="number"
@@ -138,7 +149,7 @@
                         <div className="invalid-feedback">{formik.errors.amount}</div>
                     ) : null}
                 </div>
-                <div className="col-md-2">
+                <div className="col-md-2 col-4">
                 <input
                     className={`form-control ${formik.touched.category && formik.errors.category ? 'is-invalid' : ''}`}
                     type="text"
@@ -153,7 +164,7 @@
                         <div className="invalid-feedback">{formik.errors.category}</div>
                     ) : null}
                 </div>
-                <div className="col-md-2">
+                <div className="col-md-2 col-4">
                 <input
                     className={`form-control ${formik.touched.date && formik.errors.date ? 'is-invalid' : ''}`}
                     type="date"
@@ -168,7 +179,7 @@
                         <div className="invalid-feedback">{formik.errors.date}</div>
                     ) : null}
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-3 col-12">
                 <textarea
                     className={`form-control ${formik.touched.description && formik.errors.description ? 'is-invalid' : ''}`}
                     type="text"
@@ -207,48 +218,50 @@
             <hr />
             {/* Choose between show all , only income or only expenses */}
             <div className="row my-3">
-            <div className="col-md-2 d-flex align-items-center">
-                <h4 className="h5 text-main">What type to show? </h4>
-            </div>
-            <div className="col-md-3">
-                <ul className="nav nav-pills" id="typeSwiper" role="tablist">
-                <li className="nav-item" role="presentation">
-                    <button className="nav-link active" id="all-records" data-bs-toggle="tab" data-bs-target="#all-records-pane" type="button" role="tab" aria-controls="all-records-pane" aria-selected="true" >
-                    All
-                    </button>
-                </li>
-                <li className="nav-item" role="presentation">
-                    <button className="nav-link" id="income-records" data-bs-toggle="tab" data-bs-target="#income-records-pane" type="button" role="tab" aria-controls="income-records-pane" aria-selected="false" >
-                    Incomes
-                    </button>
-                </li>
-                <li className="nav-item" role="presentation">
-                    <button className="nav-link" id="expenses-records" data-bs-toggle="tab" data-bs-target="#expenses-records-pane" type="button" role="tab" aria-controls="expenses-records-pane" aria-selected="false">
-                    Expenses
-                    </button>
-                </li>
-                </ul>
-            </div>
+                <div className="col-md-2 col-5 d-flex align-items-center">
+                    <h4 className="h5 text-main">What type to show? </h4>
+                </div>
+                <div className="col-md-3 col-7">
+                    <ul className="nav nav-pills" id="typeSwiper" role="tablist">
+                    <li className="nav-item" role="presentation">
+                        <button className="nav-link active" id="all-records" data-bs-toggle="tab" data-bs-target="#all-records-pane" type="button" role="tab" aria-controls="all-records-pane" aria-selected="true" >
+                        All
+                        </button>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                        <button className="nav-link" id="income-records" data-bs-toggle="tab" data-bs-target="#income-records-pane" type="button" role="tab" aria-controls="income-records-pane" aria-selected="false" >
+                        Incomes
+                        </button>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                        <button className="nav-link" id="expenses-records" data-bs-toggle="tab" data-bs-target="#expenses-records-pane" type="button" role="tab" aria-controls="expenses-records-pane" aria-selected="false">
+                        Expenses
+                        </button>
+                    </li>
+                    </ul>
+                </div>
             </div>
             {/* Show the records */}
             {/* The head */}
             <div className="row bg-main p-4 text-white rounded text-center">
-            <div className="col-md-2">
+            <div className="col-md-2 col-3">
                 <h3 className="h6 mb-0">Amount</h3>
             </div>
-            <div className="col-md-2">
+            <div className="col-md-2 col-3">
                 <h3 className="h6 mb-0">Category</h3>
             </div>
-            <div className="col-md-2">
-                <h3 className="h6 mb-0">Date</h3>
-            </div>
-            <div className="col-md-2">
-                <h3 className="h6 mb-0">Description</h3>
-            </div>
-            <div className="col-md-2 ">
+            {phoneMediaQuery.matches? "" : <>
+                <div className="col-md-2 col-2">
+                    <h3 className="h6 mb-0">Date</h3>
+                </div>
+                <div className="col-md-2 col-2">
+                    <h3 className="h6 mb-0">Description</h3>
+                </div>
+            </>}
+            <div className={`col-md-2 col-3`}>
                 <h3 className="h6 mb-0">Delete</h3>
             </div>
-            <div className="col-md-2 ">
+            <div className="col-md-2 col-3 ">
                 <h3 className="h6 mb-0">Modify</h3>
             </div>
             </div>
