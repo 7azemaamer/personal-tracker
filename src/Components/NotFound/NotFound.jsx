bc3099b37afb02891c2f40dcc5a640784cc51cdb
7azemaamer <zicoaamer@gmail.com>
2023-11-16 23:45:44 +0200

    updated notfound

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -4,6 +4,11 @@ import styles from './NotFound.module.css';
 export default function NotFound(){
 
     return <>
-        <h2>NotFound</h2>
+        <div className="container mt-5 d-flex justify-content-center align-items-center">
+            <div className="row text-center">
+                <h2 className="fs-0">Error 404!</h2>
+                <p>This page is not found</p>
+            </div>
+        </div>
     </>
 } 
